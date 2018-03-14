9e1cfdd22dc3934983727fc06f014d699c43fae0
Daniel Conner <dannyconner@hotmail.co.uk>
2018-03-14 14:32:27 +0000

    seeded runnerfile, and tested crud functionality on course

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,14 +2,33 @@ import db.DBHelper;
 import models.Course;
 import models.Student;
 
+import java.util.List;
+
 public class Runner {
 
     public static void main(String[] args) {
 
         Course course1 = new Course("English", "Masters");
         DBHelper.save(course1);
+        Course course2 = new Course("Geography", "HNC");
+        DBHelper.save(course2);
 
         Student student1 = new Student("Peter", 18, 12345, course1);
         DBHelper.save(student1);
+        Student student2 = new Student("Louis", 17, 79876, course2);
+        DBHelper.save(student2);
+        Student student3 = new Student("Chris", 15, 15482, course1);
+        DBHelper.save(student3);
+        Student student4 = new Student("Meg", 16, 34221, course2);
+        DBHelper.save(student4);
+
+        student4.setCourse(course1);
+        DBHelper.save(student4);
+
+        DBHelper.delete(student1);
+
+        List<Student> allStudents = DBHelper.getAll(Student.class);
+        List<Course> allCourses = DBHelper.getAll(Course.class);
+
     }
 }
--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -1,2 +1,49 @@
+import db.DBHelper;
+import models.Course;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.omg.CORBA.CODESET_INCOMPATIBLE;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
 public class CourseTest {
+
+    Course course;
+
+    @Before
+    public void setUp() {
+        course = new Course("Computing Science", "Masters");
+        DBHelper.save(course);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        DBHelper.delete(course);
+    }
+
+    @Test
+    public void canSave() {
+        List<Course> results = DBHelper.getAll(Course.class);
+        assertEquals(1, results.size());
+    }
+
+    @Test
+    public void canUpdate() {
+        Course found = DBHelper.find(Course.class, course.getId());
+        found.setLevel("PHD");
+        DBHelper.save(found);
+        found = DBHelper.find(Course.class, course.getId());
+        assertEquals("PHD", found.getLevel());
+    }
+
+    @Test
+    public void canDelete() {
+        Course found = DBHelper.find(Course.class, course.getId());
+        DBHelper.delete(found);
+        List<Course> results = DBHelper.getAll(Course.class);
+        assertEquals(0, results.size());
+    }
 }
