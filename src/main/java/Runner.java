402942cbd17fde67351cc7d49acaedf6c5824854
Daniel Conner <dannyconner@hotmail.co.uk>
2018-03-14 14:03:43 +0000

    added CRUD functions to dbhelper and getlist and getUnique reslt

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,2 +1,15 @@
+import db.DBHelper;
+import models.Course;
+import models.Student;
+
 public class Runner {
+
+    public static void main(String[] args) {
+
+        Course course1 = new Course("English", "Masters");
+        DBHelper.save(course1);
+
+        Student student1 = new Student("Peter", 18, 12345, course1);
+        DBHelper.save(student1);
+    }
 }
