802f1d64ff0b8ba8aba77943f6f138e5a087243f
Daniel Conner <dannyconner@hotmail.co.uk>
2018-03-14 13:41:09 +0000

    created sturcture to models, and updated cfg file

--- src/main/java/models/Course.java
+++ src/main/java/models/Course.java
@@ -1,4 +1,40 @@
 package models;
 
 public class Course {
+
+    private int id;
+    private String title;
+    private String level;
+
+    public Course() {
+    }
+
+    public Course(String title, String level) {
+        this.title = title;
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
 }
--- src/main/java/models/Lesson.java
+++ src/main/java/models/Lesson.java
@@ -1,4 +1,40 @@
 package models;
 
 public class Lesson {
+
+    private int id;
+    private String title;
+    private int classroomNumber;
+
+    public Lesson() {
+    }
+
+    public Lesson(String title, int classroomNumber) {
+        this.title = title;
+        this.classroomNumber = classroomNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getClassroomNumber() {
+        return classroomNumber;
+    }
+
+    public void setClassroomNumber(int classroomNumber) {
+        this.classroomNumber = classroomNumber;
+    }
 }
