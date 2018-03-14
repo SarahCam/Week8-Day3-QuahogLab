277c0c39c80e527ef94493c2401523156f4ee910
Daniel Conner <dannyconner@hotmail.co.uk>
2018-03-14 15:09:16 +0000

    tested crud functions on lesson and student

--- src/test/java/LessonTest.java
+++ src/test/java/LessonTest.java
@@ -1,2 +1,53 @@
+import db.DBHelper;
+import models.Course;
+import models.Lesson;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
 public class LessonTest {
+
+    Lesson lesson;
+    Course course;
+
+    @Before
+    public void setUp() throws Exception {
+        course = new Course("Engineering", "Bsc");
+        lesson = new Lesson("Mechanics", 203, course);
+        DBHelper.save(course);
+        DBHelper.save(lesson);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        DBHelper.delete(lesson);
+        DBHelper.delete(course);
+    }
+
+    @Test
+    public void canSave() {
+        List<Lesson> results = DBHelper.getAll(Lesson.class);
+        assertEquals(1, results.size());
+    }
+
+    @Test
+    public void canUpdate(){
+        Lesson found = DBHelper.find(Lesson.class, lesson.getId());
+        found.setClassroomNumber(202);
+        DBHelper.save(found);
+        found = DBHelper.find(Lesson.class, lesson.getId());
+        assertEquals(202 , found.getClassroomNumber());
+    }
+
+    @Test
+    public void canDelete() {
+        Lesson found = DBHelper.find(Lesson.class, lesson.getId());
+        DBHelper.delete(found);
+        List<Lesson> results = DBHelper.getAll(Lesson.class);
+        assertEquals(0, results.size());
+    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,54 @@
+import db.DBHelper;
+import models.Course;
+import models.Student;
+import models.Student;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
 public class StudentTest {
+
+    Student student;
+    Course course;
+
+    @Before
+    public void setUp() throws Exception {
+        course = new Course("Engineering", "Bsc");
+        student = new Student("Stewie", 2, 23475, course);
+        DBHelper.save(course);
+        DBHelper.save(student);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        DBHelper.delete(student);
+        DBHelper.delete(course);
+    }
+
+    @Test
+    public void canSave() {
+        List<Student> results = DBHelper.getAll(Student.class);
+        assertEquals(1, results.size());
+    }
+
+    @Test
+    public void canUpdate(){
+        Student found = DBHelper.find(Student.class, student.getId());
+        found.setAge(3);
+        DBHelper.save(found);
+        found = DBHelper.find(Student.class, student.getId());
+        assertEquals(3 , found.getAge());
+    }
+
+    @Test
+    public void canDelete() {
+        Student found = DBHelper.find(Student.class, student.getId());
+        DBHelper.delete(found);
+        List<Student> results = DBHelper.getAll(Student.class);
+        assertEquals(0, results.size());
+    }
 }
